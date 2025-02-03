0711894fc6d4bd763d8bf2dcb446c00ef256f534
Maxim Harder <maximharder@bundeswehr.org>
2025-02-03 16:22:28 +0100

    Исправление ошибок

--- upload/engine/inc/maharder/_modules/dle_faker/utils/init.php
+++ upload/engine/inc/maharder/_modules/dle_faker/utils/init.php
@@ -1,45 +1,17 @@
 <?php
 
-$composerJson     = MH_ADMIN . '/composer.json';
 $updateLock       = __DIR__ . '/composer.lock';
 $requiredPackages = [
 	"fakerphp/faker" => "*"
 ];
 
-if (file_exists($composerJson) && !file_exists($updateLock)) {
-	$composerData = json_decode(file_get_contents($composerJson), true);
+ComposerAction::requirePackage($requiredPackages, lockerFile: $updateLock);
 
-	foreach ($requiredPackages as $name => $version) {
-		if (!isset($composerData['require'][$name])) {
-			ComposerAction::require($name, $version);
-			$composerData['require'][$name] = $version;
-		}
-	}
-
-	file_put_contents($composerJson, json_encode($composerData), LOCK_EX);
-
-	try {
-		ComposerAction::update();
-	} catch (JsonException $e) {
-		LogGenerator::generateLog(
-			'DLE Faker',
-			'DleFaker/ComposerUpdate',
-				$e->getMessage()
-		);
-	}
-
-	try {
-		DataManager::createLockFile($updateLock);
-	} catch (JsonException $e) {
-
-	}
-
-	if (!class_exists('Faker\Factory')) {
-		LogGenerator::generateLog('DLE Faker', 'DleFaker/init', [
-			__('Зависимости не были установлены!'),
-			__('Установите их при помощи консоли сами! PHP должен быть выполняемым!'),
-			"<code><pre>cd " . MH_ADMIN . "</pre></code>",
-			"<code><pre>composer update</pre></code>",
-		],                        'critical');
-	}
+if (!class_exists('Faker\Factory')) {
+	LogGenerator::generateLog('DLE Faker', 'DleFaker/init', [
+		__('Зависимости не были установлены!'),
+		__('Установите их при помощи консоли сами! PHP должен быть выполняемым!'),
+		"<code><pre>cd " . MH_ADMIN . "</pre></code>",
+		"<code><pre>composer update</pre></code>",
+	],                        'critical');
 }
